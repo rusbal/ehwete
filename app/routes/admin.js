f337fc4405e95fe1d3bd6f2f4910e255c0609896
Raymond Usbal <raymond@philippinedev.com>
2018-01-10 04:18:00 +0800

    Same route for new/create, edit/update

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,17 +4,17 @@ const c = require('../helpers').c
 
 router.get('/pages/', c('Page'))
 router.get('/pages/new', c('Page#new'))
-router.post('/pages/create', c('Page#create'))
+router.post('/pages/new', c('Page#create'))
 router.get('/pages/edit/:slug', c('Page#edit'))
-router.post('/pages/update/:slug', c('Page#update'))
+router.post('/pages/edit/:slug', c('Page#update'))
 router.delete('/pages/destroy/:id', c('Page#destroy'))
 router.post('/pages/order', c('Page#reorder'))
 
 router.get('/categories/', c('Category'))
 router.get('/categories/new', c('Category#new'))
-router.post('/categories/create', c('Category#create'))
+router.post('/categories/new', c('Category#create'))
 router.get('/categories/edit/:slug', c('Category#edit'))
-router.post('/categories/update/:slug', c('Category#update'))
+router.post('/categories/edit/:slug', c('Category#update'))
 router.delete('/categories/destroy/:id', c('Category#destroy'))
 
 module.exports = router
